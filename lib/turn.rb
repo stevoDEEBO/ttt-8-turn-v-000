846bd522bf59780397f90eb437a50f289155c007
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-24 05:50:47 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
 
-  if valid_move?(board, index) == true
+  if valid_move?(board, index) #== true
     move(board, index, char)
 
   #elsif valid_move?(board, index) == false
