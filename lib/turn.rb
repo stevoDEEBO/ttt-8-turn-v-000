913020d26b23119fabe96c988ac76b745a8676b4
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-24 06:24:26 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,7 +16,7 @@ end
 
 def valid_move?(board, index)
   if index.between?(0, 8)
-    if !position_taken?(board, index) != true
+    if position_taken?(board, index) != true
       true
     end
   end
