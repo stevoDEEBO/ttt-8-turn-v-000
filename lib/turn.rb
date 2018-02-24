067ec6268812559694a0ad1a3eba76ec36524e97
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-24 05:23:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,3 +14,19 @@ def move(board, index, char = "X")
   board[index] = char
 end
 
+def valid_move?(board, index)
+  if index.between?(0, 8)
+    if position_taken?(board, index) != true
+      true
+    end
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
