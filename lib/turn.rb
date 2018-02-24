b04340978d8a4545929ee232052b1bdc2dd4a0d5
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-24 06:24:05 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,9 +23,9 @@ def valid_move?(board, index)
 end
 
 def position_taken?(board, index)
-  if board[index] != " " || board[index] == "" || board[index] == nil
-    #false
-  #elsif board[index] == "X" || board[index] == "O"
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
     true
   end
 end
